a3793b682daa08cbf6d19c971a970c97e93b354e
sahilhedau49 <hedausahil49@gmail.com>
2023-05-23 09:10:11 +0530

    Project Setup Done
    React Added
    Tailwind CSS Added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mx-6 my-2 font-extrabold text-2xl">Hello World.</h1>
+    </div>
+  );
+}
+
+export default App;
