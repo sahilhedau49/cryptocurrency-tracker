372ee8ee94f21d0b0eb311b17a218b20512b22a4
sahilhedau49 <hedausahil49@gmail.com>
2023-08-29 19:21:10 +0530

    Update DataPage.js

--- src/components/DataPage.js
+++ src/components/DataPage.js
@@ -21,7 +21,7 @@ const DataPage = () => {
   const getApiData = async () => {
     try {
       const res = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=51&page=1&sparkline=false&locale=en`
       );
       setCoins(res.data);
     } catch (error) {
